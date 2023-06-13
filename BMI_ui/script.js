d394a0d54e4bf53ee36980611b691a600aa27d50
Jimmy Carlier <jimmy.carlier@lapiscine.pro>
2023-06-13 18:32:07 +0200

    Add forEach boucle

--- BMI_ui/script.js
+++ BMI_ui/script.js
@@ -41,51 +41,67 @@
 // }
 // ---------------------------------------------------------------------
 
-const chooseWeight = document.querySelector(".weight");
-const chooseHeight = document.querySelector(".height");
-const calculImc = document.querySelector(".send");
-const score = document.querySelector(".score");
-const result = document.querySelector(".result");
+// const chooseWeight = document.querySelector(".weight");
+// const chooseHeight = document.querySelector(".height");
+// const calculImc = document.querySelector(".send");
+// const score = document.querySelector(".score");
+// const result = document.querySelector(".result");
 
-calculImc.addEventListener("click", calculator);
+// calculImc.addEventListener("click", calculator);
 
-// function calculator(){
-//     score.textContent = 0
-//     score.textContent = chooseWeight.value / ((chooseHeight.value/100) ** 2);
+// // function calculator(){
+// //     score.textContent = 0
+// //     score.textContent = chooseWeight.value / ((chooseHeight.value/100) ** 2);
+// // }
+
+// let myObject = {
+//   taille: 0,
+//   poid: 0,
+//   IMC: 0,
+// };
+
+// const BMIData = [
+//   { name: "Maigreur", color: "midnightblue", range: [0, 18.5] },
+//   { name: "Bonne santé", color: "green", range: [18.5, 25] },
+//   { name: "Surpoids", color: "lightcoral", range: [25, 30] },
+//   { name: "Obésité modérée", color: "orange", range: [30, 35] },
+//   { name: "Obésité sévère", color: "crimson", range: [35, 40] },
+//   { name: "Obésité morbide", color: "purple", range: 40 },
+// ];
+
+// function calculator() {
+//   myObject.taille = chooseHeight.value;
+//   myObject.poid = chooseWeight.value;
+//   myObject.IMC = myObject.poid / (myObject.taille / 100) ** 2;
+//   score.textContent = myObject.IMC.toFixed(2);
+//   for (i = 0; i < BMIData.length; i++) {
+//     if (
+//       myObject.IMC > BMIData[i].range[0] &&
+//       myObject.IMC <= BMIData[i].range[1]
+//     ) {
+//       result.textContent = BMIData[i].name;
+//       score.style.color = BMIData[i].color;
+//       break;
+//     } else if (myObject.IMC > BMIData[i].range) {
+//       result.textContent = BMIData[i].name;
+//       score.style.color = BMIData[i].color;
+//       break;
+//     }
+//   }
 // }
+// ---------------------------------------------------------------
+const allButtons = document.querySelectorAll(".main-btn");
+const sendColor = document.querySelector(".send");
+const myContainer = document.querySelector(".main-container");
 
-let myObject = {
-  taille: 0,
-  poid: 0,
-  IMC: 0,
-};
+let color = ["red", "blue", "green"];
 
-const BMIData = [
-  { name: "Maigreur", color: "midnightblue", range: [0, 18.5] },
-  { name: "Bonne santé", color: "green", range: [18.5, 25] },
-  { name: "Surpoids", color: "lightcoral", range: [25, 30] },
-  { name: "Obésité modérée", color: "orange", range: [30, 35] },
-  { name: "Obésité sévère", color: "crimson", range: [35, 40] },
-  { name: "Obésité morbide", color: "purple", range: 40 },
-];
-
-function calculator() {
-  myObject.taille = chooseHeight.value;
-  myObject.poid = chooseWeight.value;
-  myObject.IMC = myObject.poid / (myObject.taille / 100) ** 2;
-  score.textContent = myObject.IMC.toFixed(2);
-  for (i = 0; i < BMIData.length; i++) {
-    if (
-      myObject.IMC > BMIData[i].range[0] &&
-      myObject.IMC <= BMIData[i].range[1]
-    ) {
-      result.textContent = BMIData[i].name;
-      score.style.color = BMIData[i].color;
-      break;
-    } else if (myObject.IMC > BMIData[i].range) {
-      result.textContent = BMIData[i].name;
-      score.style.color = BMIData[i].color;
-      break;
-    }
-  }
+// for (i = 0; i < allButtons.length; i++) {
+//   allButtons[i].addEventListener("click", myFunction);
+// }
+allButtons.forEach((ele) => {
+  ele.addEventListener("click", myFunction);
+});
+function myFunction(event) {
+  myContainer.style.backgroundColor = event.target.getAttribute("data-color");
 }
