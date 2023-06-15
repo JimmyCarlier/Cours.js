185c637a755cd7d9854dcad196406f36e632d28f
Jimmy Carlier <jimmy.carlier@lapiscine.pro>
2023-06-15 15:20:33 +0200

    Final exercice

--- DOM_interaction/script.js
+++ DOM_interaction/script.js
@@ -69,21 +69,98 @@ function concat() {
 // ------------------------------------------------------
 // Exo 5
 
-// const range = document.querySelector(".input-5");
-// const resultRange = document.querySelector(".result-5");
-
-// resultRange.addEventListener("input", value);
-
-// function value(event) {
-//   resultRange.innerHTML = event.target.value;
-// }
-
-
 const range = document.querySelector(".input-5");
 const resultRange = document.querySelector(".result-5");
 
-resultRange.innerHTML = range.value;
+range.addEventListener("input", value);
 
-range.addEventListener('input', (event)=>{
-    resultRange.innerHTML = event.target.value
-})
+function value(event) {
+  resultRange.textContent = event.target.value;
+}
+
+// const range = document.querySelector(".input-5");
+// const resultRange = document.querySelector(".result-5");
+
+// resultRange.innerHTML = range.value;
+
+// range.addEventListener('input', (event)=>{
+//     resultRange.innerHTML = event.target.value
+// })
+
+// ---------------------------------------------------------
+// Exo 6
+const select = document.querySelector("#language-select");
+const resultOfOption = document.querySelector(".result-6");
+
+select.addEventListener("change", valu);
+
+const associations = {
+  js: "coucou js",
+  python: "coucou python",
+  php: "coucou php",
+};
+
+function valu(e) {
+  // resultOfOption.textContent = select.value
+  resultOfOption.textContent = associations[e.currentTarget.value];
+  console.log(e);
+}
+// ----------------------------------------------------------------------
+// exo 7
+
+const color = document.querySelector(".input-7");
+const colorResult = document.querySelector(".result-7");
+
+color.addEventListener("input", funColor);
+
+function funColor() {
+  colorResult.style.backgroundColor = color.value;
+}
+// ---------------------------------------------------------------------
+// exo 8
+const btn8 = document.querySelectorAll(".btn-8");
+const resultColor = document.querySelector(".result-8");
+
+btn8.forEach((e) => {
+  e.addEventListener("click", zizi);
+});
+
+function zizi(e) {
+  resultColor.style.backgroundColor = e.target.getAttribute("data-color");
+}
+// -----------------------------------------------------------------------
+// exo 9
+
+const devMusic = document.querySelectorAll(".checkbox-9");
+const resultCheck = document.querySelector(".result-9");
+
+let table = [];
+
+devMusic.forEach((e) => {
+  e.addEventListener("click", test);
+});
+
+initValue = resultCheck.textContent;
+function test(e) {
+
+  // if (e.target.checked) {
+  //   resultCheck.textContent = "";
+  //   table.push(e.target.value);
+  //   resultCheck.textContent = table;
+  // } else {
+  //   table.pop(e.target.value);
+  //   resultCheck.textContent = table;
+  // }
+
+  let myStr = ""
+  if(devMusic[0].checked){
+    myStr += devMusic[0].value
+  }
+
+  if(devMusic[1].checked){
+    if(myStr) myStr += "-"
+    myStr += devMusic[1].value
+  }
+
+  resultCheck.textContent = myStr
+}
